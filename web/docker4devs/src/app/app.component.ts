a5dbd6d2f72bf37ad4660ffa5c3553b1daa28969
Dillon <dillon-a@hotmail.com>
2018-08-22 09:30:00 -0500

    updating component to display service return values

--- web/docker4devs/src/app/app.component.ts
+++ web/docker4devs/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ValueService } from './value.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  values: Observable<string[]>;
+
+  constructor(private valueService: ValueService) {
+    this.values = valueService.getValues();
+  }
 }
